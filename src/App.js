a1defb23a8fb4e2d42e71aa5fe559435d41a19fc
jamesjo <jamescakr@gmail.com>
2024-08-25 11:37:08 -0600

    redux assignment

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Box from "./component/Box";
+
+function App() {
+  let count = useSelector((state) => state.count);
+  let id = useSelector((state) => state.id);
+  let password = useSelector((state) => state.password);
+  let dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({ type: "increment", payload: { num: 5 } });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "decrement", payload: { num: 5 } });
+  };
+
+  // const login = () => {
+  //   dispatch({ type: "LOGIN", payload: { id: "noona", password: "123" } });
+  // };
+
+  return (
+    <div>
+      <h1>
+        {id},{password}
+      </h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>Increment</button>
+      <Box />
+      {/* <button onClick={login}>Login</button> */}
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
